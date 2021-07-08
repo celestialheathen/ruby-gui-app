8506fdba9ef2c38a06bdb6fb7dce8d16752f81e5
celestialheathen <jingchaoyu@hotmail.com>
2021-07-08 11:45:14 -0400

    clear button

--- hello_world.rb
+++ hello_world.rb
@@ -1,10 +1,17 @@
 require 'tk'
 
-root = TkRoot.new(title: "Hello, World!", padx: 200, pady: 200)
+root = TkRoot.new(title: "Hello, World!", padx: 250, pady: 250)
 
-TkLabel.new(root) do
-  text "Hello, World!"
-  pack { padx 15; pady 15; side 'left' }
+display = TkLabel.new(root) do
+  text "helllloo"
+  pack { padx 15; pady 15; side 'top' }
+end 
+
+
+TkButton.new(root) do
+  text("Clear")
+  pack { padx 15; pady 15; side 'buttom'}
+  command(lambda {display.configure('text', '')})
 end 
 
 Tk.mainloop
