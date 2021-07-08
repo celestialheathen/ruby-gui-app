9f936425b26c033587b37aad492f3f1d309925bd
celestialheathen <jingchaoyu@hotmail.com>
2021-07-08 16:40:51 -0400

    grid

--- hello_world.rb
+++ hello_world.rb
@@ -1,20 +1,33 @@
 require 'tk'
 
-root = TkRoot.new(title: "Hello, World!", padx: 250, pady: 250)
+root = TkRoot.new(title: "Hello, World!")
 
 display = TkLabel.new(root) do
   text "helllloo"
   height 2
   width 60
   background "gray"
-  pack
+  grid(row: 0, column: 0, columnspan: 3)
+end 
+
+seven_btn = TkButton.new(root) do 
+  text("7")
+  grid(row: 1, column: 0)
+end 
+eight_btn = TkButton.new(root) do 
+  text("8")
+  grid(row: 1, column: 1)
+end 
+nine_btn = TkButton.new(root) do 
+  text("7")
+  grid(row: 1, column: 2)
 end 
 
 
-TkButton.new(root) do
+clear_btn = TkButton.new(root) do
   text("Clear")
-  pack { padx 15; pady 15; side 'buttom'}
   command(lambda {display.configure('text', '')})
+  grid(row: 2, column: 0)
 end 
 
 Tk.mainloop
