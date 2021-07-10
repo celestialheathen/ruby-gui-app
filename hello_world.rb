84e98ee8e41bc22662b7ffd776f6990c665e5002
celestialheathen <jingchaoyu@hotmail.com>
2021-07-10 12:35:04 -0400

    added functionality btns

--- hello_world.rb
+++ hello_world.rb
@@ -3,11 +3,11 @@ require 'tk'
 root = TkRoot.new(title: "Ruby Calculator")
 
 display = TkLabel.new(root) do
-  text "12"
+  text ""
   height 2
   width 50
   background "gray"
-  grid(row: 0, column: 0, columnspan: 3)
+  grid(row: 0, column: 0, columnspan: 4)
 end 
 
 current_value = display.cget('text')
@@ -26,7 +26,7 @@ eight_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '8')})
+  command(lambda {display.configure('text', current_value.concat('8'))})
 end 
 nine_btn = TkButton.new(root) do 
   text("9")
@@ -34,7 +34,15 @@ nine_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '9')})
+  command(lambda {display.configure('text', current_value.concat('9'))})
+end 
+multiply_btn = TkButton.new(root) do 
+  text("X")
+  grid(row: 1, column: 3)
+  borderwidth 1
+  padx 30
+  pady 25
+  command()
 end 
 
 four_btn = TkButton.new(root) do 
@@ -43,7 +51,7 @@ four_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '4')})
+  command(lambda {display.configure('text', current_value.concat('4'))})
 end 
 five_btn = TkButton.new(root) do 
   text("5")
@@ -51,7 +59,7 @@ five_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '5')})
+  command(lambda {display.configure('text', current_value.concat('5'))})
 end 
 six_btn = TkButton.new(root) do 
   text("6")
@@ -59,7 +67,15 @@ six_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '6')})
+  command(lambda {display.configure('text', current_value.concat('6'))})
+end 
+minus_btn = TkButton.new(root) do 
+  text("-")
+  grid(row: 2, column: 3)
+  borderwidth 1
+  padx 30
+  pady 25
+  command()
 end 
 
 one_btn = TkButton.new(root) do 
@@ -68,7 +84,7 @@ one_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '1')})
+  command(lambda {display.configure('text', current_value.concat('1'))})
 end 
 two_btn = TkButton.new(root) do 
   text("2")
@@ -76,7 +92,7 @@ two_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '2')})
+  command(lambda {display.configure('text', current_value.concat('2'))})
 end 
 three_btn = TkButton.new(root) do 
   text("3")
@@ -84,14 +100,62 @@ three_btn = TkButton.new(root) do
   borderwidth 1
   padx 30
   pady 25
-  command(lambda {display.configure('text', '3')})
+  command(lambda {display.configure('text', current_value.concat('3'))})
+end 
+plus_btn = TkButton.new(root) do 
+  text("+")
+  grid(row: 3, column: 3)
+  borderwidth 1
+  padx 30
+  pady 25
+  command()
 end 
 
+negative_btn = TkButton.new(root) do 
+  text("+/-")
+  grid(row: 4, column: 0)
+  borderwidth 1
+  padx 30
+  pady 25
+  command(lambda {display.configure('text', current_value * -1)})
+end 
+zero_btn = TkButton.new(root) do 
+  text("0")
+  grid(row: 4, column: 1)
+  borderwidth 1
+  padx 30
+  pady 25
+  command(lambda {display.configure('text', current_value.concat('0'))})
+end 
+dot_btn = TkButton.new(root) do 
+  text(".")
+  grid(row: 4, column: 2)
+  borderwidth 1
+  padx 30
+  pady 25
+  command(lambda {display.configure('text', '.')})
+end 
+equal_btn = TkButton.new(root) do 
+  text("=")
+  grid(row: 4, column: 3)
+  borderwidth 1
+  padx 30
+  pady 25
+  command()
+end 
 
+divide_btn = TkButton.new(root) do 
+  text("/")
+  grid(row: 5, column: 2)
+  borderwidth 1
+  padx 30
+  pady 25
+  command()
+end 
 clear_btn = TkButton.new(root) do
   text("Clear")
   command(lambda {display.configure('text', '')})
-  grid(row: 4, column: 0)
+  grid(row: 5, column: 3)
 end 
 
 Tk.mainloop
