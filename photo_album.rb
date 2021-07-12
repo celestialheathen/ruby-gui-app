2258dd1342f9618f28b70dd80ee82ff55730da18
celestialheathen <jingchaoyu@hotmail.com>
2021-07-12 15:05:49 -0400

    added functional btns

--- photo_album.rb
+++ photo_album.rb
@@ -8,6 +8,7 @@ image2 = TkPhotoImage.new(file: 'images/dayne.jpeg')
 image3 = TkPhotoImage.new(file: 'images/matt.jpeg')
 image4 = TkPhotoImage.new(file: 'images/max.jpeg')
 image5 = TkPhotoImage.new(file: 'images/will.jpeg')
+image6 = TkPhotoImage.new(file: 'images/jahaira.png')
 
 images_array = [image1, image2, image3, image4, image5]
 current_index = 0
@@ -28,7 +29,7 @@ back_btn = TkButton.new(root) do
 end 
 
 label = TkLabel.new(root) do
-  text "Image 1 of 1"
+  text "Image 1 of 5"
   pady 20
   grid(row: 1, column: 1)
 end 
@@ -38,7 +39,10 @@ next_btn = TkButton.new(root) do
   padx 15
   pady 15
   grid(row: 1, column: 2)
-  command( lambda { container.configure('image', images_array[current_index += 1]) })
+  command( lambda { 
+    container.configure('image', images_array[current_index += 1]) 
+    label.configure('text', "Image #{current_index + 1} of #{images_array.length}")
+  })
 end 
 
 Tk.mainloop
