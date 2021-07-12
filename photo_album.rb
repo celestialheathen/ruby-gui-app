24390d09e51c4f1de847fbebd7889a04c154ca5e
celestialheathen <jingchaoyu@hotmail.com>
2021-07-12 14:49:46 -0400

    added functional btns

--- photo_album.rb
+++ photo_album.rb
@@ -3,10 +3,17 @@ require 'tkextlib/tkimg'
 
 root = TkRoot.new(title: 'Photo Album')
 
-image = TkPhotoImage.new(file: 'images/will.jpeg')
+image1 = TkPhotoImage.new(file: 'images/aaron.jpeg')
+image2 = TkPhotoImage.new(file: 'images/dayne.jpeg')
+image3 = TkPhotoImage.new(file: 'images/matt.jpeg')
+image4 = TkPhotoImage.new(file: 'images/max.jpeg')
+image5 = TkPhotoImage.new(file: 'images/will.jpeg')
+
+images_array = [image1, image2, image3, image4, image5]
+current_index = 0
 
 container = TkLabel.new(root) do 
-  image image
+  image images_array[current_index]
   padx 250
   pady 250
   grid(row: 0, column: 0, columnspan: 3)
@@ -17,6 +24,7 @@ back_btn = TkButton.new(root) do
   padx 15
   pady 15
   grid(row: 1, column: 0)
+  command( lambda { container.configure('image', images_array[current_index -= 1]) })
 end 
 
 label = TkLabel.new(root) do
@@ -25,11 +33,12 @@ label = TkLabel.new(root) do
   grid(row: 1, column: 1)
 end 
 
-forward_btn = TkButton.new(root) do 
+next_btn = TkButton.new(root) do 
   text ">"
   padx 15
   pady 15
   grid(row: 1, column: 2)
+  command( lambda { container.configure('image', images_array[current_index += 1]) })
 end 
 
 Tk.mainloop
