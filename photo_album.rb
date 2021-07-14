42c92c7a3fc5322eaa3f34d1a2fd871825e35d46
celestialheathen <jingchaoyu@hotmail.com>
2021-07-14 14:48:50 -0400

    clean up

--- photo_album.rb
+++ photo_album.rb
@@ -3,20 +3,19 @@ require 'tkextlib/tkimg'
 
 root = TkRoot.new(title: 'Photo Album')
 
-image1 = TkPhotoImage.new(file: 'images/aaron.jpeg', height: 400, width: 400)
+image1 = TkPhotoImage.new(file: 'images/aaron.jpeg')
 image2 = TkPhotoImage.new(file: 'images/dayne.jpeg')
 image3 = TkPhotoImage.new(file: 'images/matt.jpeg')
 image4 = TkPhotoImage.new(file: 'images/max.jpeg')
 image5 = TkPhotoImage.new(file: 'images/will.jpeg')
-# image6 = TkPhotoImage.new(file: 'images/jahaira.png')
 
 images_array = [image1, image2, image3, image4, image5]
 current_index = 0
 
 container = TkLabel.new(root) do 
   image images_array[current_index]
-  # padx 250
-  # pady 250
+  padx 250
+  pady 250
   grid(row: 0, column: 0, columnspan: 3)
 end 
 
